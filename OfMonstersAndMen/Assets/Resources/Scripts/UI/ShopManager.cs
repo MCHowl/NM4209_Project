6ea2e03199a51fa514a26e2674699b1a596056d8
Matthew Chan <_mattc@hotmail.com>
2019-03-25 17:53:44 +0800

    ADD upgrade mechanics and revised shop

--- OfMonstersAndMen/Assets/Resources/Scripts/UI/ShopManager.cs
+++ OfMonstersAndMen/Assets/Resources/Scripts/UI/ShopManager.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopManager : MonoBehaviour {
 
 	Canvas shopCanvas;
+	UpgradeManager upgradeManager;
 	GameController gameController;
 	MonsterManager monsterManager;
 	LandManager landManager;
 
+	public Image MonsterPortrait;
+	public TextMeshProUGUI MonsterName;
+	public TextMeshProUGUI MonsterStrength;
+	public TextMeshProUGUI MonsterAgility;
+	public TextMeshProUGUI MonsterDefence;
+	public TextMeshProUGUI MonsterHealth;
+
+	public Button BuyButton;
+	public Button UnlockButton;
+
+	int selectedIndex;
+	GameObject selectedMonster;
+
 	void Start() {
+		upgradeManager = GetComponent<UpgradeManager>();
 		gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
 		monsterManager = GameObject.FindGameObjectWithTag("MonsterManager").GetComponent<MonsterManager>();
 		landManager = GameObject.FindGameObjectWithTag("LandManager").GetComponent<LandManager>();
@@ -18,9 +35,9 @@ public class ShopManager : MonoBehaviour {
 		CloseShop();
     }
 
-	public void BuyUnit(GameObject monster) {
-		if (gameController.SpendMana(monster.GetComponent<Unit>().Mana)) {
-			monsterManager.CreateUnit(monster);
+	public void BuyUnit() {
+		if (gameController.SpendMana(selectedMonster.GetComponent<Unit>().Mana)) {
+			monsterManager.CreateUnit(selectedMonster);
 		}
 	}
 
@@ -51,11 +68,69 @@ public class ShopManager : MonoBehaviour {
 		}
 	}
 
+	public void BuyUpgrade() {
+		int temp = selectedIndex;
+		if (gameController.SpendMana(upgradeManager.unlockCost[selectedIndex])) {
+			upgradeManager.isUnlocked[selectedIndex] = true;
+
+			ResetShop();
+			SetShop(temp);
+		}
+	}
+
 	public void OpenShop() {
 		shopCanvas.enabled = true;
 	}
 
 	public void CloseShop() {
+		ResetShop();
 		shopCanvas.enabled = false;
+	}
+
+	public void SetShop(int i) {
+		selectedIndex = i;
+		selectedMonster = upgradeManager.UnitUpgrades[i];
+		Unit monster = selectedMonster.GetComponent<Unit>();
+
+		MonsterPortrait.sprite = monster.UnitProtrait;
+		MonsterName.text = monster.UnitName;
+		MonsterStrength.text = monster.UnitStats.Strength.ToString();
+		MonsterAgility.text = monster.UnitStats.Agility.ToString();
+		MonsterDefence.text = monster.UnitStats.Defence.ToString();
+		MonsterHealth.text = monster.Health.ToString();
+
+		if (upgradeManager.isUnlocked[i]) {
+			BuyButton.enabled = true;
+			BuyButton.GetComponentInChildren<TextMeshProUGUI>().text = "Buy (" + monster.Mana.ToString() + ")";
+		} else {
+			bool canUnlock = true;
+			foreach (int check in upgradeManager.precedingUpgrades[i]) {
+				print("Checking upgrade " + check.ToString() + ", result - " + upgradeManager.isUnlocked[check]);
+				if (!upgradeManager.isUnlocked[check]) {
+					canUnlock = false;
+				}
+			}
+			if (canUnlock) {
+				UnlockButton.enabled = true;
+				UnlockButton.GetComponentInChildren<TextMeshProUGUI>().text = "Unlock (" + upgradeManager.unlockCost[i].ToString() + ")";
+			}
+		}
+	}
+
+	public void ResetShop() {
+		MonsterName.text = "";
+		MonsterStrength.text = "";
+		MonsterAgility.text = "";
+		MonsterDefence.text = "";
+		MonsterHealth.text = "";
+
+		MonsterPortrait.sprite = null;
+
+		BuyButton.enabled = false;
+		BuyButton.GetComponentInChildren<TextMeshProUGUI>().text = "---";
+		UnlockButton.enabled = false;
+		UnlockButton.GetComponentInChildren<TextMeshProUGUI>().text = "---";
+
+		selectedMonster = null;
 	}
 }
---
+++ OfMonstersAndMen/Assets/Resources/Scripts/UI/UpgradeManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+	public GameObject[] UnitUpgrades;
+	public bool[] isUnlocked;
+	public int[] unlockCost;
+	public List<int>[] precedingUpgrades;
+	public List<int>[] followingUpgrades;
+
+	private void Awake() {
+		InitialiseUnlocks();
+		InitialiseUnlockCosts();
+		InitialisePrecedingUpgrades();
+		InitialiseFollowingUpgrades();
+	}
+
+	void InitialiseUnlocks() {
+		isUnlocked = new bool[15];
+		for (int i = 0; i < isUnlocked.Length; i++) {
+			if (i % 5 == 0) {
+				isUnlocked[i] = true;
+			} else {
+				isUnlocked[i] = false;
+			}
+		}
+	}
+
+	void InitialiseUnlockCosts() {
+		unlockCost = new int[15];
+		for (int i = 0; i < unlockCost.Length; i++) {
+			if (i % 5 == 0) {
+				unlockCost[i] = 0;
+			} else if (i % 5 == 1 || i % 5 == 2) {
+				unlockCost[i] = 50;
+			} else {
+				unlockCost[i] = 100;
+			}
+		}
+	}
+
+	void InitialisePrecedingUpgrades() {
+		precedingUpgrades = new List<int>[15];
+		precedingUpgrades[0] = new List<int> { };
+		precedingUpgrades[1] = new List<int> {0};
+		precedingUpgrades[2] = new List<int> {0};
+		precedingUpgrades[3] = new List<int> {1};
+		precedingUpgrades[4] = new List<int> {2};
+		precedingUpgrades[5] = new List<int> { };
+		precedingUpgrades[6] = new List<int> {5};
+		precedingUpgrades[7] = new List<int> {5};
+		precedingUpgrades[8] = new List<int> {6};
+		precedingUpgrades[9] = new List<int> {7};
+		precedingUpgrades[10] = new List<int> { };
+		precedingUpgrades[11] = new List<int> {10};
+		precedingUpgrades[12] = new List<int> {10};
+		precedingUpgrades[13] = new List<int> {11};
+		precedingUpgrades[14] = new List<int> {12};
+	}
+
+	void InitialiseFollowingUpgrades() {
+		followingUpgrades = new List<int>[15];
+		followingUpgrades[0] = new List<int> {1, 2};
+		followingUpgrades[1] = new List<int> {3};
+		followingUpgrades[2] = new List<int> {4};
+		followingUpgrades[3] = new List<int> { };
+		followingUpgrades[4] = new List<int> { };
+		followingUpgrades[5] = new List<int> {6, 7};
+		followingUpgrades[6] = new List<int> {8};
+		followingUpgrades[7] = new List<int> {9};
+		followingUpgrades[8] = new List<int> { };
+		followingUpgrades[9] = new List<int> { };
+		followingUpgrades[10] = new List<int> {11, 12};
+		followingUpgrades[11] = new List<int> {13};
+		followingUpgrades[12] = new List<int> {14};
+		followingUpgrades[13] = new List<int> { };
+		followingUpgrades[14] = new List<int> { };
+	}
+}
